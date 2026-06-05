f2ff89e077550dbfb5a6cf9f697854836be7f6e5
Ezehebubechidubem <140657875+Ezehebubechidubem@users.noreply.github.com>
2026-06-05 10:22:53 +0100

    Create exam/waec.js

---
+++ Backend/provider-catalog/exam/waec.js
@@ -0,0 +1,4 @@
+module.exports = [
+  { variation_code: "waec-registration", name: "WAEC Registration PIN", variation_amount: 4000 },
+  { variation_code: "waec-result-checker", name: "WAEC Result Checker PIN", variation_amount: 1500 }
+];
