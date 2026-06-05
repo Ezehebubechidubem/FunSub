ec1c086107d41b1aaec0cdb78fc11d719f40a348
Ezehebubechidubem <140657875+Ezehebubechidubem@users.noreply.github.com>
2026-06-05 10:18:06 +0100

    Create gotv.js

---
+++ Backend/provider-catalog/cable-tv/gotv.js
@@ -0,0 +1,6 @@
+module.exports = [
+  { variation_code: "gotv-smallie", name: "GOtv Smallie", variation_amount: 1575 },
+  { variation_code: "gotv-jinja", name: "GOtv Jinja", variation_amount: 3300 },
+  { variation_code: "gotv-jolli", name: "GOtv Jolli", variation_amount: 4850 },
+  { variation_code: "gotv-max", name: "GOtv Max", variation_amount: 8500 }
+];
