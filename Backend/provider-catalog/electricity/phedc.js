ca55e5c0677508ca1a9ff7628dd58a65821852ea
Ezehebubechidubem <140657875+Ezehebubechidubem@users.noreply.github.com>
2026-06-05 10:20:51 +0100

    Create phedc.js

---
+++ Backend/provider-catalog/electricity/phedc.js
@@ -0,0 +1,4 @@
+module.exports = [
+  { variation_code: "ibedc-prepaid", name: "IBEDC Prepaid", variation_amount: 1000 },
+  { variation_code: "ibedc-postpaid", name: "IBEDC Postpaid", variation_amount: 2000 }
+];
