29dc6e1850e90598b7547cc6d4b970d095ff06b5
Ezehebubechidubem <140657875+Ezehebubechidubem@users.noreply.github.com>
2026-06-05 10:23:18 +0100

    Create neco.js

---
+++ Backend/provider-catalog/exam/neco.js
@@ -0,0 +1,4 @@
+module.exports = [
+  { variation_code: "neco-registration", name: "NECO Registration PIN", variation_amount: 4000 },
+  { variation_code: "neco-result-checker", name: "NECO Result Checker PIN", variation_amount: 1500 }
+];
