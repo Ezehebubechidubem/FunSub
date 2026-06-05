43a4b2de786e4c51a4e64cdfa2141cf5fed29970
Ezehebubechidubem <140657875+Ezehebubechidubem@users.noreply.github.com>
2026-06-05 10:09:34 +0100

    Create 9mobile.js

---
+++ Backend/provider-catalog/data/9mobile.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { variation_code: "9mobile-500mb", name: "9MOBILE 500MB", variation_amount: 300 },
+  { variation_code: "9mobile-1gb", name: "9MOBILE 1GB", variation_amount: 500 },
+  { variation_code: "9mobile-2gb", name: "9MOBILE 2GB", variation_amount: 900 },
+  { variation_code: "9mobile-3gb", name: "9MOBILE 3GB", variation_amount: 1300 },
+  { variation_code: "9mobile-5gb", name: "9MOBILE 5GB", variation_amount: 2300 }
+];
