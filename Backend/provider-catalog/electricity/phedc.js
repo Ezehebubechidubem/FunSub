316c6a03dafa88693917abf83087df6c001c815b
Ezehebubechidubem <140657875+Ezehebubechidubem@users.noreply.github.com>
2026-06-05 10:36:54 +0100

    Update phedc.js

--- Backend/provider-catalog/electricity/phedc.js
+++ Backend/provider-catalog/electricity/phedc.js
@@ -1,4 +1,4 @@
 module.exports = [
-  { variation_code: "phedc-prepaid", name: "PHEDC Prepaid", variation_amount: 1000 },
-  { variation_code: "phedc-postpaid", name: "PHEDC Postpaid", variation_amount: 2000 }
+  { variation_code: "phedc-prepaid", name: "PHEDC Prepaid", variation_amount: 10000 },
+  { variation_code: "phedc-postpaid", name: "PHEDC Postpaid", variation_amount: 15000 }
 ];
