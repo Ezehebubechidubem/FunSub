065308d299d2338a6881a0e163821e389bfee4e5
Ezehebubechidubem <140657875+Ezehebubechidubem@users.noreply.github.com>
2026-06-05 10:08:06 +0100

    Create glo.js

---
+++ Backend/provider-catalog/data/glo.js
@@ -0,0 +1,8 @@
+module.exports = [
+  { variation_code: "glo-500mb", name: "GLO 500MB", variation_amount: 300 },
+  { variation_code: "glo-1gb", name: "GLO 1GB", variation_amount: 450 },
+  { variation_code: "glo-2gb", name: "GLO 2GB", variation_amount: 800 },
+  { variation_code: "glo-3gb", name: "GLO 3GB", variation_amount: 1200 },
+  { variation_code: "glo-5gb", name: "GLO 5GB", variation_amount: 2000 },
+  { variation_code: "glo-10gb", name: "GLO 10GB", variation_amount: 3500 }
+];
