650f5eef9db1e36e4b259133fafb60c453fc0106
Ezehebubechidubem <140657875+Ezehebubechidubem@users.noreply.github.com>
2026-06-05 10:08:52 +0100

    Create airtel.js

---
+++ Backend/provider-catalog/data/airtel.js
@@ -0,0 +1,8 @@
+module.exports = [
+  { variation_code: "airtel-500mb", name: "AIRTEL 500MB", variation_amount: 300 },
+  { variation_code: "airtel-1gb", name: "AIRTEL 1GB", variation_amount: 500 },
+  { variation_code: "airtel-2gb", name: "AIRTEL 2GB", variation_amount: 900 },
+  { variation_code: "airtel-3gb", name: "AIRTEL 3GB", variation_amount: 1300 },
+  { variation_code: "airtel-5gb", name: "AIRTEL 5GB", variation_amount: 2300 },
+  { variation_code: "airtel-10gb", name: "AIRTEL 10GB", variation_amount: 4500 }
+];
