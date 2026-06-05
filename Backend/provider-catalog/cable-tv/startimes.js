c616bf8728f798626d282522bf0818888208d844
Ezehebubechidubem <140657875+Ezehebubechidubem@users.noreply.github.com>
2026-06-05 10:18:41 +0100

    Create startimes.js

---
+++ Backend/provider-catalog/cable-tv/startimes.js
@@ -0,0 +1,5 @@
+module.exports = [
+  { variation_code: "startimes-nova", name: "Startimes Nova", variation_amount: 1700 },
+  { variation_code: "startimes-smart", name: "Startimes Smart", variation_amount: 2700 },
+  { variation_code: "startimes-super", name: "Startimes Super", variation_amount: 4100 }
+];
