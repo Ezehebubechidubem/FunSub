fe64e474574cf4522ae4773337166486025b9a7b
Ezehebubechidubem <140657875+Ezehebubechidubem@users.noreply.github.com>
2026-06-05 10:23:49 +0100

    Create jamb.js

---
+++ Backend/provider-catalog/exam/jamb.js
@@ -0,0 +1,4 @@
+module.exports = [
+  { variation_code: "jamb-pin", name: "JAMB e-PIN", variation_amount: 6000 },
+  { variation_code: "jamb-regularization", name: "JAMB Regularization PIN", variation_amount: 6000 }
+];
