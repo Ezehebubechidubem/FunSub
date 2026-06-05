bb600bc71f65f74c108ba56e3332821e21094ca2
Ezehebubechidubem <140657875+Ezehebubechidubem@users.noreply.github.com>
2026-06-05 10:07:14 +0100

    Create data/mtn.js

---
+++ Backend/provider-catalog/data/mtn.js
@@ -0,0 +1,8 @@
+module.exports = [
+  { variation_code: "mtn-500mb", name: "MTN 500MB", variation_amount: 300 },
+  { variation_code: "mtn-1gb", name: "MTN 1GB", variation_amount: 500 },
+  { variation_code: "mtn-2gb", name: "MTN 2GB", variation_amount: 1000 },
+  { variation_code: "mtn-3gb", name: "MTN 3GB", variation_amount: 1500 },
+  { variation_code: "mtn-5gb", name: "MTN 5GB", variation_amount: 2500 },
+  { variation_code: "mtn-10gb", name: "MTN 10GB", variation_amount: 5000 }
+];
